9b9f3f80ad0932ba0f268247ea8e9b600c16af85
kantm <kantm@DESKTOP-H26FR5O>
2023-08-09 18:48:30 +0200

    Change AdminSeeder logic and rename to UserSeeder

---
+++ erpool-homework-app/database/seeders/UserSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => 'admin',
+                'role' => 'admin',
+            ],
+            [
+                'name' => 'Mate Kantor',
+                'email' => 'kant.mate@gmail.com',
+                'password' => 'password',
+                'role' => 'standard',
+            ]
+        ];
+
+        foreach($users as $user){
+            $created_user = User::create([
+                'name' => $user['name'],
+                'email' => $user['email'],
+                'password' => Hash::make($user['password']),
+            ]);
+
+            $created_user->assignRole($user['role']);
+        }
+    }
+}
