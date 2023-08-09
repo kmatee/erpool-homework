6d7be509de175bbdb3c5acbbb42f2119e9bd4ca4
kantm <kantm@DESKTOP-H26FR5O>
2023-08-09 18:18:58 +0200

    Delete is_admin column from users table

--- erpool-homework-app/database/migrations/2023_08_09_161739_delete_isadmin_column_from_users_table.php
+++ erpool-homework-app/database/migrations/2023_08_09_161739_delete_isadmin_column_from_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_admin')->after('email')->default(false);
+            $table->dropColumn('is_admin');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('is_admin');
+            $table->boolean('is_admin')->after('email')->default(false);
         });
     }
 };
