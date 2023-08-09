1d078e5157f5441aff78c17d3ffb8b79681a6f37
kantm <kantm@DESKTOP-H26FR5O>
2023-08-09 16:02:02 +0200

    Create category seeder to make 4 categories in database

---
+++ erpool-homework-app/database/seeders/CategorySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categories')->insert([
+            'name' => 'extreme'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'hiking'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'swimming'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'ball'
+        ]);
+    }
+}
