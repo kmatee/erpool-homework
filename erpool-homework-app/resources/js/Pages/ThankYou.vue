49fad8aef323e680510a7c55e97930fb630f69cb
kantm <kantm@DESKTOP-H26FR5O>
2023-08-15 00:49:02 +0200

    Add simple thank you page

---
+++ erpool-homework-app/resources/js/Pages/ThankYou.vue
@@ -0,0 +1,14 @@
+<template>
+    <AppLayout title="Thank you for your order">
+        <div>
+            <p class="uppercase font-semibold ">Thank you for your order!</p>
+        </div>
+    </AppLayout>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/AppLayout.vue';
+export default {
+    components: {AppLayout}
+}
+</script>
