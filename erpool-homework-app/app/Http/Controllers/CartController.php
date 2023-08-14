725d9267e0872ae7ea105dd79d1b6ad613434cf6
kantm <kantm@DESKTOP-H26FR5O>
2023-08-14 21:03:07 +0200

    Add index method to CartController

--- erpool-homework-app/app/Http/Controllers/CartController.php
+++ erpool-homework-app/app/Http/Controllers/CartController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use Inertia\Inertia;
 
 class CartController extends Controller
 {
+
+    public function index()
+    {
+        $user_id = auth()->user()->id;
+        \Cart::session($user_id);
+        $items = \Cart::getContent();
+        return Inertia::render('ShoppingCart', [
+            'items' => $items,
+        ]);
+    }
+
     public function addToCart(Request $request)
     {
         if (!auth()->check()) {
@@ -15,15 +28,21 @@ class CartController extends Controller
             return redirect()->route('home');
         }
         else {
-            $form = [
-                'id' => $request->input('product_id'),
-                'name' => $request->input('product_name'),
-                'price' => $request->input('product_price')
-            ];
+            $user_id = auth()->user()->id;
+            $product = Product::find($request->input('product_id'));
+
+            \Cart::session($user_id)->add(array(
+                'id' => $product->id,
+                'name' => $product->name,
+                'price' => $product->price,
+                'quantity' => 1,
+                'attributes' => array('image' => $product->image_url),
+            ));
+            
             session()->flash('flash.banner','Item added to your cart');
             session()->flash('flash.bannerStyle', 'success');
+            
             return redirect()->route('home');
-
             
             //dd($form);
 
