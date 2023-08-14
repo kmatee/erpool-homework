280af7101c9564e6796493f1f2617e4f9363a5dc
kantm <kantm@DESKTOP-H26FR5O>
2023-08-14 18:12:52 +0200

    Add form that posts product data to the CartController

--- erpool-homework-app/resources/js/Pages/Home.vue
+++ erpool-homework-app/resources/js/Pages/Home.vue
@@ -1,12 +1,16 @@
 <script setup>
-import { ref, defineEmits, defineProps } from 'vue';
-import { Head, Link, router } from '@inertiajs/vue3';
+import { ref, defineEmits, defineProps, reactive } from 'vue';
+import { Head, Link, router, useForm } from '@inertiajs/vue3';
 import ApplicationMark from '@/Components/ApplicationMark.vue';
 import Banner from '@/Components/Banner.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+//import { ZiggyVue } from 'ziggy-vue';
+import route from '@/../../vendor/tightenco/ziggy/src/js/index';
+
+
 
 defineProps({
     canLogin: Boolean,
@@ -17,29 +21,47 @@ defineProps({
     products: Array,
 });
 
-const emits = defineEmits([
-    'addToCart',
-])
 
 const showingNavigationDropdown = ref(false);
 
-const handleAddToCart = (productId) => {
-    console.log('Adding product to cart:', productId);
-}
+/* const form = useForm({
+                product_id: null,
+            }); */
+
+/* const submit = () => {
+    form.post(route('cart.add'))
+}; */
+
+
 </script>
 
 <script>
-import AddToShoppingCart from '@/Components/AddToShoppingCart.vue';
+
     export default {
-        components: {
-            AddToShoppingCart,
+        data() {
+            return {
+                form: {
+                    product_id: null,
+
+                }
+            }
+        },
+        methods: {
+            submit(productId, productName, productPrice) {
+                this.form.product_id = productId;
+                this.form.product_name = productName;
+                this.form.product_price = productPrice;
+
+                this.$inertia.post('/cart', this.form);
+            }
         },
     };
 </script>
 
 <template>
     <Head title="Home page" />
-    <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+    <Banner/>
+    <div v-if="canLogin" class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-right">
         <Link v-if="$page.props.auth.user" :href="route('dashboard')"
             class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-500 dark:hover:text-blue hidden sm:block">
         Dashboard</Link>
@@ -168,7 +190,24 @@ import AddToShoppingCart from '@/Components/AddToShoppingCart.vue';
               <div class="flex items-center justify-between p-4">
                 <span class="text-xl text-gray-700">{{ prod.price }} Ft</span>
               </div>
-              <AddToShoppingCart :product-id="prod.id" @add-to-cart="handleAddToCart(prod.id)"/>
+              <form @submit.prevent="submit(prod.id, prod.name, prod.price)">
+                <input type="hidden" name="product_id" v-model="form.product_id"/>
+                <input type="hidden" name="product_name" v-model="form.product_name"/>
+                <input type="hidden" name="product_price" v-model="form.product_price"/>
+                <button type="submit" class="bg-white p-2 rounded-full relative">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="256" height="256" viewBox="0 0 256 256" xml:space="preserve"
+                        class="w-5 h-5 absolute bottom-2 left-2 bg-white hover:bg-green-200 rounded-full">
+                        <defs>
+                        </defs>
+                        <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
+                            <path d="M 72.975 58.994 H 31.855 c -1.539 0 -2.897 -1.005 -3.347 -2.477 L 15.199 13.006 H 3.5 c -1.933 0 -3.5 -1.567 -3.5 -3.5 s 1.567 -3.5 3.5 -3.5 h 14.289 c 1.539 0 2.897 1.005 3.347 2.476 l 13.309 43.512 h 36.204 l 10.585 -25.191 h -6.021 c -1.933 0 -3.5 -1.567 -3.5 -3.5 s 1.567 -3.5 3.5 -3.5 H 86.5 c 1.172 0 2.267 0.587 2.915 1.563 s 0.766 2.212 0.312 3.293 L 76.201 56.85 C 75.655 58.149 74.384 58.994 72.975 58.994 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                            <circle cx="28.88" cy="74.33" r="6.16" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform="  matrix(1 0 0 1 0 0) "/>
+                            <circle cx="74.59" cy="74.33" r="6.16" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform="  matrix(1 0 0 1 0 0) "/>
+                            <path d="M 62.278 19.546 H 52.237 V 9.506 c 0 -1.933 -1.567 -3.5 -3.5 -3.5 s -3.5 1.567 -3.5 3.5 v 10.04 h -10.04 c -1.933 0 -3.5 1.567 -3.5 3.5 s 1.567 3.5 3.5 3.5 h 10.04 v 10.04 c 0 1.933 1.567 3.5 3.5 3.5 s 3.5 -1.567 3.5 -3.5 v -10.04 h 10.041 c 1.933 0 3.5 -1.567 3.5 -3.5 S 64.211 19.546 62.278 19.546 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                        </g>
+                    </svg>
+                </button>
+              </form>
             </div>
         </div>
       </div>
